3ab20fbddc8b7076188d9f26c47d0067b9602e9f
Berke Enes Aktümen <81863556+berkeenesakt@users.noreply.github.com>
2023-07-28 16:31:02 +0300

    Code is formatted

--- lib/interactive_maps_marker.dart
+++ lib/interactive_maps_marker.dart
@@ -16,8 +16,11 @@ class MarkerItem {
   double latitude;
   double longitude;
 
-  MarkerItem(
-      {required this.id, required this.latitude, required this.longitude});
+  MarkerItem({
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+  });
 }
 
 class InteractiveMapsMarker extends StatefulWidget {
@@ -80,6 +83,8 @@ class InteractiveMapsMarker extends StatefulWidget {
     }
     return state;
   }
+
+  void Function(CameraPosition)? get onCameraMove => onCameraMove;
 }
 
 class InteractiveMapsMarkerState extends State<InteractiveMapsMarker> {
@@ -90,6 +95,7 @@ class InteractiveMapsMarkerState extends State<InteractiveMapsMarker> {
   Set<Marker> markers = {};
   int currentIndex = 0;
   ValueNotifier selectedMarker = ValueNotifier<int?>(0);
+
   @override
   void initState() {
     rebuildMarkers(currentIndex);
@@ -150,7 +156,7 @@ class InteractiveMapsMarkerState extends State<InteractiveMapsMarker> {
             myLocationEnabled: true,
             myLocationButtonEnabled: false,
             onMapCreated: _onMapCreated,
-            onCameraMove: widget.onCameraMove,
+            onCameraMove: onCameraMove,
             initialCameraPosition: CameraPosition(
               target: widget.center,
               zoom: widget.zoom,
