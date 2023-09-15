165146d97a351ed7468629ddeb63a7fb84c79970
Ghassen Ben Zahra <ghassen.benzahra@gmail.com>
2023-09-15 17:16:48 +0100

    add request permission for position

--- lib/interactive_maps_marker.dart
+++ lib/interactive_maps_marker.dart
@@ -108,8 +108,38 @@ class InteractiveMapsMarkerState extends State<InteractiveMapsMarker> {
     rebuildMarkers(currentIndex);
     super.didChangeDependencies();
   }
+   Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+              'Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
   void _getUserLocation() async {
+    final hasPermission = await _handleLocationPermission();
+
+    if (!hasPermission) return;
     var position = await GeolocatorPlatform.instance.getCurrentPosition(
         locationSettings: const LocationSettings(
             accuracy: LocationAccuracy.bestForNavigation));
